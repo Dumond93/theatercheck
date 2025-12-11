b73ae12722a585e21c377f6fe6d2ea63555a3665
dumond98 <andre.dumond@hotmail.com>
2025-12-11 15:29:32 +0000

    first commit

---
+++ theatercheck.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+# Andre Dumond
+# API Theater Script
+
+import requests
+from bs4 import BeautifulSoup
+import time
+import subprocess
+from datetime import datetime
+from plexapi.server import PlexServer
+import sys
+
+# Get the plex information for the plex server and login using token
+baseurl = sys.argv[1]
+token = sys.argv[2]
+plex = PlexServer(baseurl, token)
+
+# Get the plex movies library
+MovieLibrary = plex.library.section(sys.argv[3]) # Get Movies Library
+
+currentYear = datetime.now().year
+previousYear = currentYear - 1
+
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    ENDC = '\033[0m'
+
+def getCollection(collectionVideos):
+	MovieLibrary = plex.library.section('Movies')
+	try:
+		theaterCollection = MovieLibrary.collection('In Theaters')
+	except:
+		theaterCollection = MovieLibrary.createCollection('In Theaters', items=collectionVideos, smart=False, limit=None, libtype=None, sort=None, filters=None)
+		hub = theaterCollection.visibility()
+		hub.updateVisibility(recommended=True, home=True, shared=True)
+		hub.reload()
+		print ("Theater collection does not exist, creating collection...")
+	return theaterCollection
+
+def getNowPlaying():
+	print ("Getting now playing movies from https://www.fandango.com/movies-in-theaters")
+	nowPlayingMovies = []
+	URL = "https://www.fandango.com/movies-in-theaters"
+	page = requests.get(URL)
+
+	soup = BeautifulSoup(page.content, "html.parser")
+
+	elements = soup.find_all('span', class_='heading-style-1 browse-movielist--title poster-card--title')
+
+	for element in elements:
+		movieString = str(element.text)[:-7]
+		nowPlayingMovies.append(movieString)
+	return nowPlayingMovies
+
+def getMoviesFromPlex(nowPlaying):
+	print ("Getting items from plex libaray for comparison")
+	collectionVideos = []
+	for i in nowPlaying:
+		for video in plex.search(i):
+			if video.type == "movie":
+				if video.title == i:
+					if video.year == currentYear or video.year == previousYear:
+						collectionVideos.append(video)
+	return collectionVideos
+
+def getMoviesInCollection(theaterCollection):
+	moviesInCollection = theaterCollection.items()
+	return moviesInCollection
+
+
+def addMoviesToCollection(theaterCollection, collectionVideos, moviesInCollection):
+	finalAddedMovies= []
+	for item in collectionVideos:
+		if item in moviesInCollection:
+			continue
+		else:
+			finalAddedMovies.append(item)
+			print (f"{Colors.GREEN}added {Colors.ENDC}" + item.title)
+
+	if len(finalAddedMovies) != 0:
+		theaterCollection.addItems(finalAddedMovies)
+		print ("added " + str(len(finalAddedMovies)) + " items to collection")
+
+def removeMoviesFromCollection(theaterCollection, moviesInCollection, collectionVideos):
+	finalRemovedMovies= []
+	for item in moviesInCollection:
+		if item in collectionVideos:
+			continue
+		else:
+			finalRemovedMovies.append(item)
+
+	if len(finalRemovedMovies) != 0:
+		theaterCollection.removeItems(finalRemovedMovies)
+
+# Main
+print ("Theater Checker running")
+while True:
+	nowPlaying = getNowPlaying()
+	collectionVideos = getMoviesFromPlex(nowPlaying)
+	theaterCollection = getCollection(collectionVideos)
+	moviesInCollection = getMoviesInCollection(theaterCollection)
+	addMoviesToCollection(theaterCollection, collectionVideos, moviesInCollection)
+	removeMoviesFromCollection(theaterCollection, moviesInCollection, collectionVideos)
+	print ("Collection items added successfully")
+	print ("Waiting " +sys.argv[4]+ " seconds before checking again...")
+	time.sleep(int(sys.argv[4]))
